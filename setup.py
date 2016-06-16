b22b73023a707b8ed1046edf8817b8310c313168
kalidus <guillem.db@gmail.com>
2016-06-16 14:30:12 +0200

    dependency fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1a3',
+    version='0.0.1a4',
 
     description='Framework for interactive dashboards framework for the jupyter notebook',
     long_description=long_description,
